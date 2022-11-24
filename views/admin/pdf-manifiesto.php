51014f0c20667ccaedcc541a16bc974a4d4b2efd
AngeloOz <angelo-mjz@hotmail.com>
2022-11-24 01:21:03 -0500

    cambios en transportista

--- views/admin/pdf-manifiesto.php
+++ views/admin/pdf-manifiesto.php
@@ -1,13 +1,10 @@
 <?php
-require_once 'vendor/autoload.php';
 // reference the Dompdf namespace
+require_once 'vendor/autoload.php';
 use Dompdf\Dompdf;
-// $url = trim($_GET['url'], '/');
-// $infoID = "world";
-// $ruta = explode('/' , $url);
-// if(isset($ruta[2])){
-//     $infoID = $ruta[2];
-// }
+
+// $url = trim($_GET["url"],'/');
+// $url = explode('/',$url)[2];
 
 ob_start();
 require_once("modules/pdf-manifiesto-unico.php");
@@ -17,12 +14,10 @@ $dompdf = new Dompdf();
 $dompdf->loadHtml($template);
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+$dompdf->setPaper('A3', 'landscape');
 
 // Render the HTML as PDF
 $dompdf->render();
 
 // Output the generated PDF to Browser
 $dompdf->stream("",['Attachment' => false]);
-
-?>
