0f86688a180e2db646b9cc37fc87fb89a9d535fb
MILEWID <milewid@gmail.com>
2022-11-24 19:06:57 -0500

    cambios en menu dinamico

--- views/admin/layout/sidebar-productor.php
+++ views/admin/layout/sidebar-productor.php
@@ -1,26 +1,32 @@
 <li class="nav-item <?php echo $pagina == 'dashboard' ? 'active' : '' ?>">
     <a class="nav-link" href="<?php echo URL_BASE_APP; ?>admin/dashboard">
         <i class="fa fa-home menu-icon"></i>
-        <span class="menu-title">Opcion admin 1</span>
+        <span class="menu-title">Administracion</span>
     </a>
 </li>
 <li class="nav-item <?php echo $pagina == 'usuario' ? 'active' : '' ?>">
     <a class="nav-link " href="<?php echo URL_BASE_APP; ?>admin/usuario">
         <i class="fa fa-user menu-icon"></i>
-        <span class="menu-title">Opcion admin 2</span>
+        <span class="menu-title">Tu cuenta </span>
     </a>
 </li>
 <li class="nav-item <?php echo $pagina == 'datos-empresa' ? 'active' : '' ?>">
     <a class="nav-link" href="<?php echo URL_BASE_APP; ?>admin/informacion-empresa">
         <i class="fa fa-building menu-icon"></i>
-        <span class="menu-title">Opcion admin 3</span>
+        <span class="menu-title">Datos Empresa</span>
+    </a>
+</li>
+<li class="nav-item <?php echo $pagina == 'datos-empresa' ? 'active' : '' ?>">
+    <a class="nav-link" href="<?php echo URL_BASE_APP; ?>admin/instalacion-empresa">
+        <i class="fa fa-industry menu-icon"></i>
+        <span class="menu-title">Instalaciones</span>
     </a>
 </li>
 <li class="nav-item d-none d-lg-block">
     <a class="nav-link" data-toggle="collapse" href="#sidebar-layouts" aria-expanded="false"
         aria-controls="sidebar-layouts">
         <i class="fas fa-clipboard-list menu-icon"></i>
-        <span class="menu-title">Manifiesto Único</span>
+        <span class="menu-title">Manifiesto</span>
         <i class="menu-arrow"></i>
     </a>
     <div class="collapse" id="sidebar-layouts">
@@ -35,7 +41,7 @@
 <li class="nav-item">
     <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
         <i class="far fa-user menu-icon"></i>
-        <span class="menu-title">Transportistas</span>
+        <span class="menu-title">AEE</span>
         <i class="menu-arrow"></i>
     </a>
     <div class="collapse" id="ui-basic">
---
+++ views/admin/modules/instalaciones-empresa.php
@@ -0,0 +1,60 @@
+<div class="page-header">
+</div>
+<div class="card">
+  <div class="card-body">
+    <h4 class="card-title">Listado de Instalaciones</h4>
+    <div class="row">
+      <div class="col-12">
+        <div class="table-responsive">
+          <table id="order-listing" class="table">
+            <thead>
+              <tr>
+                <th>Codigo</th>
+                <th>Nombre de Instalacion </th>
+                <th>Domicilio </th>
+                <th> N ONU </th>
+                <th>Telefono </th>
+                <th>Acciones </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>EIC13</td>
+                <td>La Ganga </td>
+                <td>Cdta juan 23 Guaranda  </td>
+                <td>e345 </td>
+                <td>09947844848</td>
+                <td>acciones </td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+<!-- Modal Mostrar RAEE del manifiesto   -->
+<div class="modal fade" id="modal-aee" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel-2" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel-2">AEE a transportar</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <p>Modal body text goes here.</p>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-success">Submit</button>
+                          <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
