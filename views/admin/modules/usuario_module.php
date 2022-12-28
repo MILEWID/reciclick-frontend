d4061c713b8f7a716856f76688548a0b70dfaeca
AngeloOz <angelo-mjz@hotmail.com>
2022-12-27 23:16:19 -0500

    feat: actualizar usuario v1

--- views/admin/modules/usuario_module.php
+++ views/admin/modules/usuario_module.php
@@ -10,7 +10,7 @@ $usuarioBase = $_SESSION["userLogged"]->usuario;
             <div class="container mb-3">
                 <div class="row">
                     <div class="col-9">
-                        <input type="file" name="" id="inputFilePhoto" hidden="hidden" accept="image/*">
+                        <input type="file" id="inputFilePhoto" hidden="hidden" accept="image/*">
                         <div class="container-photo-profile">
                             <div class="pictureUser">
                                 <img src="<?php echo $_SESSION["userLoggedProfile"] ?>" alt="">
@@ -27,15 +27,15 @@ $usuarioBase = $_SESSION["userLogged"]->usuario;
             </div>
             <form class="forms-sample" id="formUsuario">
                 <div class="form-group border-top pt-4">
-                    <label for="exampleInputConfirmPassword1">Rol</label>
+                    <label for="">Rol</label>
                     <div class="row">
                         <div class="col-9">
-                            <input disabled type="text" class="form-control" id="inputNames" value="<?php echo $_SESSION["userLoggedRolText"] ?>">
+                            <input disabled type="text" class="form-control" value="<?php echo $_SESSION["userLoggedRolText"] ?>">
                         </div>
                     </div>
                 </div>
                 <div class="form-group border-top pt-4">
-                    <label for="exampleInputConfirmPassword1">Username</label>
+                    <label for="inputUserName">Username</label>
                     <div class="row">
                         <div class="col-9">
                             <input disabled type="text" class="form-control" id="inputUserName" value="<?php echo $usuarioBase->usuario; ?> ">
@@ -43,10 +43,10 @@ $usuarioBase = $_SESSION["userLogged"]->usuario;
                     </div>
                 </div>
                 <div class="form-group border-top pt-4">
-                    <label for="inputNames">Identificacion:</label>
+                    <label for="inputIdentificacion">Identificacion:</label>
                     <div class="row">
                         <div class="col-9">
-                            <input disabled type="text" class="form-control" id="inputUserName" placeholder="3243223" value="<?php echo $usuarioBase->identificacion; ?> ">
+                            <input disabled type="text" class="form-control" id="inputIdentificacion" placeholder="17XXXXXXX7" value="<?php echo $usuarioBase->identificacion; ?> ">
                         </div>
                         <div class="col-3">
                             <button type="button" data-action-button="edit" class="btn btn-primary ">editar</button>
@@ -68,7 +68,7 @@ $usuarioBase = $_SESSION["userLogged"]->usuario;
                     <label for="inputEmail">Email</label>
                     <div class="row">
                         <div class="col-9">
-                            <input disabled type="email" class="form-control" id="inputEmail" placeholder="example@gmail.com" value="<?php echo $usuarioBase->correo; ?>">
+                            <input disabled type="email" class="form-control" id="inputEmail" placeholder="example@dominio.com" value="<?php echo $usuarioBase->correo; ?>">
                         </div>
                         <div class="col-3">
                             <button type="button" data-action-button="edit" class="btn btn-primary ">editar</button>
@@ -88,6 +88,8 @@ $usuarioBase = $_SESSION["userLogged"]->usuario;
                         </div>
                     </div>
                 </div>
+
+                <input type="hidden" value="<?php echo $usuarioBase->id_tipo; ?>" id="inputIdUser">
             </form>
         </div>
     </div>
