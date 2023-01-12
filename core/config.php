4b23cce86f367f6ca265e60469fd54dd7f3604bd
AngeloOz <angelo-mjz@hotmail.com>
2023-01-11 21:13:57 -0500

    cambio de api y deploy

--- core/config.php
+++ core/config.php
@@ -7,11 +7,11 @@ $url = substr($urlPath, strlen($folderPath));
 define("URL", $url);
 define("URL_BASE_APP", "http://localhost/reciclick-frontend/");
 //micriservicio de autenticacion 
-define("URL_API_M1", "https://microservicios-p49tw.ondigitalocean.app/api/v1");
+define("URL_API_M1", "https://angello-midev.store/api/v1");
 // microservicio de empresa productora
-define("URL_API_M2", "https://microservicios-p49tw.ondigitalocean.app/api/v1");
+define("URL_API_M2", "https://angello-midev.store/api/v1");
 // microservicio de transporte
-define("URL_API_M3", "https://microservicios-p49tw.ondigitalocean.app/api/v1");
+define("URL_API_M3", "https://angello-midev.store/api/v1");
 // microservicio de almacenamiento 
-define("URL_API_M4", "https://microservicios-p49tw.ondigitalocean.app/api/v1");
+define("URL_API_M4", "https://angello-midev.store/api/v1");
 session_start();
